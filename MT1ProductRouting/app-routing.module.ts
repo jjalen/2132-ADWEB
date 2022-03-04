e9bb5676c13bab57cb001ed3e6b11a2497ed9536
Jalen Relativo <83753020+jjalen@users.noreply.github.com>
2022-03-04 21:08:19 +0800

    Update app-routing.module.ts

--- MT1ProductRouting/app-routing.module.ts
+++ MT1ProductRouting/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ClientComponent } from './client/client.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { InventoryComponent } from './inventory/inventory.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductsComponent } from './products/products.component';
+import { ReactiveComponent } from './reactive/reactive.component';
 
 const routes: Routes = [{path: 'about', component:AboutComponent},
                         {path: 'contact', component:ContactComponent},
                         {path: 'home', component:HomeComponent},
                         {path: 'products', component:ProductsComponent},
                         {path: 'product-details', component:ProductDetailsComponent},
+                        {path: 'reactive', component:ReactiveComponent},
+                        {path: 'inventory', component:InventoryComponent},
+                        {path: 'client' , component:ClientComponent},
                         {path: '**', component:PagenotfoundComponent}];
 
 @NgModule({
@@ -19,4 +25,5 @@ const routes: Routes = [{path: 'about', component:AboutComponent},
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, AboutComponent, ProductsComponent, ProductDetailsComponent, ContactComponent, PagenotfoundComponent]
+export const routingComponents = [HomeComponent, AboutComponent, ProductsComponent, ProductDetailsComponent, ContactComponent, InventoryComponent, ClientComponent, PagenotfoundComponent]
+
